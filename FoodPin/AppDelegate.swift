3b6f2e699fe135da4ccc98b2b034edd53e0ef725
Emese Toth <emesetoth@Davids-MBP.lan>
2019-04-08 21:09:14 +0100

    This was CH22 as well

--- FoodPin/AppDelegate.swift
+++ FoodPin/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let container = NSPersistentContainer(name: "FoodPin")
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
         // Customize the back button
         let backButtonImage = UIImage(named: "back")
