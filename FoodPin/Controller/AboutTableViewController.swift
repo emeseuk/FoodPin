7e0e6c329756ad5e512e2bff47527898cddd6d69
Emese Toth <emesetoth@Davids-MBP.lan>
2019-04-09 13:20:29 +0100

    Updated About Storyboard

---
+++ FoodPin/Controller/AboutTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  AboutTableViewController.swift
+//  FoodPin
+//
+//  Created by Emese Toth on 08/04/2019.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class AboutTableViewController: UITableViewController {
+
+    var sectionTitles = ["Feedback", "Follow Us"]
+    var sectionContent = [[(image: "store", text: "Rate us on App Store", link: "https://www.apple.com/itunes/charts/paid-apps/"),
+        (image: "chat", text: "Tell us your feedback", link: "http://www.appcoda.com/contact")],
+        [(image: "twitter", text: "Twitter", link: "https://twitter.com/appcodamobile"),
+        (image: "facebook", text: "Facebook", link: "https://facebook.com/appcodamobile"),
+        (image: "instagram", text: "Instagram", link: "https://www.instagram.com/appcodadotcom")]]
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Configure navigation bar appearance
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default
+        )
+        navigationController?.navigationBar.shadowImage = UIImage()
+        if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont ]
+        }
+        tableView.tableFooterView = UIView()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return sectionTitles.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return sectionContent[section].count
+    }
+
+     override func tableView(_ tableView: UITableView, titleForHeaderInSection section:Int) -> String? {
+        return sectionTitles[section]
+     }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AboutCell", for: indexPath)
+        
+        // Configure the cell...
+        let cellData = sectionContent[indexPath.section][indexPath.row]
+        cell.textLabel?.text = cellData.text
+        cell.imageView?.image = UIImage(named: cellData.image)
+        return cell
+    }
+ 
+
+}
